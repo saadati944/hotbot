e38c1e8564c031102f9827b9c8fce2eada250ee2
Ali Saadati <saadati944@gmail.com>
2021-05-06 10:44:20 +0430

    add sample settings.py file

---
+++ sample_settings.py
@@ -0,0 +1,9 @@
+Token = "YOUR_BOT_TOKEN"
+
+Start_message = """
+a message to show to the user when the command /start is issued
+"""
+
+Help_message = """
+a message to show to the user when the command /help is issued
+"""
